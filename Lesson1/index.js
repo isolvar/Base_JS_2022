a4da205d0d041f01185be7beab6773a9d4232b52
Alexey Shemanov <schemanov2012@yandex.ru>
2022-02-19 11:20:42 +0300

    added first script

---
+++ Lesson1/index.js
@@ -0,0 +1,12 @@
+console.log("Hello JavaScript!");
+
+const outPlanetName = "Earth";
+let currentUserTime;
+let articleCount = 10;
+let isPaymentCash = true;
+
+let userName = "Alexey";
+let userSurname = "Shemanov";
+let userPatronymic = "Vladimirovich";
+
+console.log()
